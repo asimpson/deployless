b213a8ddafa862d248e6d46b834e7e0c699c669d
Adam Simpson <adam@heysparkbox.com>
2017-10-09 09:45:49 -0400

    feat: add ability to deploy arbitrary directories along with files

--- utils/zip.js
+++ utils/zip.js
@@ -1,10 +1,18 @@
 const shell = require('shelljs');
+const fs = require('fs');
 
-const zip = (args) => {
+const zip = args => {
   console.log('λ zipping...');
   const key = args.key || `${args.name}.zip`;
   const file = args.file || `${args.name}.js`;
-  shell.exec(`zip -r ${key} ${file} ${args.files.join(' ')} node_modules`, { silent: true });
+  const dirs = args.files.filter(x => fs.statSync(x).isDirectory());
+  const files = args.files.filter(x => !fs.statSync(x).isDirectory());
+  shell.exec(`zip ${key} ${file} ${files.join(' ')}`, {
+    silent: true,
+  });
+  shell.exec(`zip -ru ${key} ${dirs.join(' ')} node_modules`, {
+    silent: true,
+  });
 };
 
 module.exports = zip;
